34736894e1123c68cdb21461bb4ec7b03b237686
RERE CHINTYA <rerechintya@student.telkomuniversity.ac.id>
2022-05-31 18:30:08 +0700

    Update CounterTest.java

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -9,21 +9,21 @@ public class CounterTest {
 	@Test
 	public void testReset() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 0; i <10; i++) {
-			testCounter.increment();
+
+		for (int i = 0; i < 10; i++) {
+			testCounter.incrementT();
 		}
-		
+
 		testCounter.reset();
-		
+
 		assertEquals(testCounter.getCount(), 0);
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
 		}
@@ -32,8 +32,8 @@ public class CounterTest {
 	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
 		}
